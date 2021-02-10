15bb8ff9490269a56aa09729d6077335cc3898df
amkolotov <amkolotov@mail.ru>
2021-02-10 17:39:29 +0500

    templates

--- books/mainapp/views.py
+++ books/mainapp/views.py
@@ -19,6 +19,7 @@ class BooksView(Years, ListView):
     """Вывод полного списка книг"""
     model = Book
     queryset = Book.objects.filter(draft=False)
+    ordering = ['-year']
 
     # def get_context_data(self, *, object_list=None, **kwargs):
     #     context = super().get_context_data()
